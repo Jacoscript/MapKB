5c9b1ef97c14cebd0cf54cdcb35572cfcaf793c1
Fry, Tanner (Contractor) Edward <tfry@contractor.usgs.gov>
2019-12-12 12:38:54 -0600

    Fixed sub-tabs not placing correctly.

--- scripts/menu_styles.js
+++ scripts/menu_styles.js
@@ -1,12 +1,34 @@
+// Tanner Fry
+// tfry@contractor.usgs.gov
+// This script is used for the general stylizing and user interaction with the
+// menu for the MAKB web page.
+
 function pageLoad(jQuery) {
-	// Hover sub tabs for Add Marker
+	handleSubTabs();
+};
+
+// #################### //
+// ### Sub-tab code ### //
+// #################### //
+
+// Handle sub-tabs
+// A function that makes sure that sub-tabs are open only when they are
+// interacted by their parent tab.
+function handleSubTabs(){
+	// TODO: Grab position of parent tab and set margin based off of parent tab
+	
+	// Below doesn't work
+	// $("#add-marker").html("Add Marker " + $(".dropbtn").offset().top);
+	
+	
+	// Hover sub-tabs for Add Marker
 	// Syntax: .mouseenter(eventdata, function);
 	$("#add-marker").mouseenter("add-marker", showSubTabs);
 	$("#add-marker").mouseleave("add-marker", hideSubTabs);
 	$("#sub-add-marker").mouseenter("add-marker", showSubTabs);
 	$("#sub-add-marker").mouseleave("add-marker", hideSubTabs);
 	
-	// Hover sub tabs for Del Marker
+	// Hover sub-tabs for Del Marker
 	// Syntax: .mouseenter(eventdata, function);
 	$("#del-marker").mouseenter("del-marker", showSubTabs);
 	$("#del-marker").mouseleave("del-marker", hideSubTabs);
@@ -14,7 +36,6 @@ function pageLoad(jQuery) {
 	$("#sub-del-marker").mouseleave("del-marker", hideSubTabs);
 };
 
-// Show subtabs
 function showSubTabs(specificTab) {
 	$("#sub-" + specificTab.data).css("display", "block");
 };
