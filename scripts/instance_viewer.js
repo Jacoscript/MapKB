d03ef94baa51a2a457df35cd8dee8316cd24a5c6
Fry, Tanner (Contractor) Edward <tfry@contractor.usgs.gov>
2020-01-02 16:57:40 -0600

    +- Updated to apply more objects around a specified object within the instance viewer. Still needs more work.

--- scripts/instance_viewer.js
+++ scripts/instance_viewer.js
@@ -33,44 +33,81 @@ function Canvas_Object() {
         // Create objects and their locations based on the number of objects
         // NOTE: 360 / n = degrees between objects
         // NOTE: 1 object is the main object of the instance
-        var degrees_between_objs = 360 / num_objs_for_inst;
+        var tier_1_obj_limit = 10
+        if (num_objs_for_inst > tier_1_obj_limit) {
+            var tier_1_degrees_between_objs = 360 / tier_1_obj_limit;
+            var tier_2_degrees_between_objs = 360 
+                / (num_objs_for_inst - tier_1_obj_limit);
+        } else {
+            var tier_1_degrees_between_objs = 360 / num_objs_for_inst;
+            var tier_2_degrees_between_objs = 360 / num_objs_for_inst;
+        }
         if (num_objs_for_inst > 0) {
+            // Customizable object design
             var tier_1_radius_circle = 50
             var tier_1_radius_line_outer = 36
             var tier_1_radius_line_inner = 14
+            var tier_2_radius_circle = 100
+            var tier_2_radius_line_outer = 86
+            var tier_2_radius_line_inner = 14
             // Draw center object
             this.drawCircleForObject(150, 150, 10, 0, 2 * Math.PI);
 
-            // Draw other objects
+            // Draw other objects dynamically based on the amount given
             for (var i = 0; i < num_objs_for_inst; i++) {
-                // Modify x and y based on degrees_between_objects and the
-                // current object being created
-                // X := originX + cos(angle) * radius
-                // Y := originY + sin(angle) * radius
-                var radians = degrees_between_objs * Math.PI / 180;
-                x = 150 + Math.cos(radians * (i + 1)) * tier_1_radius_circle;
-                y = 150 + Math.sin(radians * (i + 1)) * tier_1_radius_circle;
-                this.drawCircleForObject(x, y, 10, 0, 2 * Math.PI);
-                
-                // Draw relations between objects
-                x1 = 150 + Math.cos(radians * (i + 1)) 
-                    * tier_1_radius_line_outer;
-                y1 = 150 + Math.sin(radians * (i + 1)) 
-                    * tier_1_radius_line_outer;
-                x2 = 150 + Math.cos(radians * (i + 1)) 
-                    * tier_1_radius_line_inner;
-                y2 = 150 + Math.sin(radians * (i + 1)) 
-                    * tier_1_radius_line_inner;
-                this.drawRelationLineBetweenObjects(x1, y1, x2, y2);
+                if (i < tier_1_obj_limit) {
+                    // Draw tier 1 objects that are connected to the main
+                    // object that was selected
+
+                    // Modify x and y based on degrees_between_objects and the
+                    // current object being created
+                    // X := originX + cos(angle) * radius
+                    // Y := originY + sin(angle) * radius
+                    var radians = tier_1_degrees_between_objs * Math.PI / 180;
+                    x = 150 + Math.cos(radians * (i + 1)) * tier_1_radius_circle;
+                    y = 150 + Math.sin(radians * (i + 1)) * tier_1_radius_circle;
+                    this.drawCircleForObject(x, y, 10, 0, 2 * Math.PI);
+                    
+                    // Draw relations between objects
+                    x1 = 150 + Math.cos(radians * (i + 1)) 
+                        * tier_1_radius_line_outer;
+                    y1 = 150 + Math.sin(radians * (i + 1)) 
+                        * tier_1_radius_line_outer;
+                    x2 = 150 + Math.cos(radians * (i + 1)) 
+                        * tier_1_radius_line_inner;
+                    y2 = 150 + Math.sin(radians * (i + 1)) 
+                        * tier_1_radius_line_inner;
+                    this.drawRelationLineBetweenObjects(x1, y1, x2, y2);
+                } else {
+                    // Draw tier 2 objects that are connected to the main
+                    // object that was selected
+                    var radian_offset = 18 * Math.PI / 180;  // FIX: Only works on 15 objects
+                    var radians = tier_2_degrees_between_objs * Math.PI / 180;
+                    x = 150 + Math.cos(radians * (i + 1) + radian_offset) * tier_2_radius_circle;
+                    y = 150 + Math.sin(radians * (i + 1) + radian_offset) * tier_2_radius_circle;
+                    this.drawCircleForObject(x, y, 10, 0, 2 * Math.PI);
+                    
+                    // Draw relations between objects
+                    x1 = 150 + Math.cos(radians * (i + 1) + radian_offset) 
+                        * tier_2_radius_line_outer;
+                    y1 = 150 + Math.sin(radians * (i + 1) + radian_offset) 
+                        * tier_2_radius_line_outer;
+                    x2 = 150 + Math.cos(radians * (i + 1) + radian_offset) 
+                        * tier_2_radius_line_inner;
+                    y2 = 150 + Math.sin(radians * (i + 1) + radian_offset) 
+                        * tier_2_radius_line_inner;
+                    this.drawRelationLineBetweenObjects(x1, y1, x2, y2);
+                }
             }
 
             // TODO: Create handles for object information in instance viewer
+            // such as object name, and relations between other objects
 
         } else {
             // Center object
             this.drawCircleForObject(150, 150, 10, 0, 2 * Math.PI);
             // TODO: Create handles for object information in instance viewer
-
+            // NOTE: Would only need object name and similar info for this sec
         }
     }
 
@@ -114,6 +151,6 @@ function testInstanceLoop (amount_objects) {
 
 $(document).ready(function() {
     var cObj = new Canvas_Object();
-    cObj.createInstanceViewer(10);
-    //testInstanceLoop(8);
+    //cObj.createInstanceViewer(15);
+    testInstanceLoop(15);
 });
