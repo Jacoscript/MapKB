83f8185fa0331cf762084efe6903f7a6d17df1aa
Fry, Tanner (Contractor) Edward <tfry@contractor.usgs.gov>
2020-03-16 14:28:08 -0500

    +- Switched to boostrap.

--- scripts/logging/notification-manager.js
+++ scripts/logging/notification-manager.js
@@ -11,7 +11,7 @@ class ManagerNotification {
 		this.loop_time = 2;  // time in hours how long the loop should continue
 		this.loop_notification_index = 1;
 		this.notification_queue = [];
-		this.notification_speed = 400;
+		this.notification_speed = 600;
 		this.notification_toggle = false; // False = turned off
 	}
 
@@ -20,12 +20,12 @@ class ManagerNotification {
 		// of the notification and then adds the given content to a list of
 		// notifications waiting to be displayed.
 	
-		if (type == "Error" || type == "Other" ||type == "Success" || type == "Warning") {
+		if (type == 'Error' || type == 'Other' ||type == 'Success' || type == 'Warning') {
 			this.notification_queue.push([type, content]);
 			// Add to 'In Queue' dropdown item in the notifications tab
-			$("#notification-in-queue").html("In Queue (" + this.notification_queue.length + ")");
+			$('#notification-in-queue').html('In Queue (' + this.notification_queue.length + ')');
 		} else {
-			this.showNotification("Error", "Notification '" + type + "' was not of type 'Error', 'Other', 'Success' or 'Warning'.");
+			this.showNotification('Error', 'Notification "' + type + '" was not of type "Error", "Other", "Success" or "Warning".');
 		}
 	}
 
@@ -56,40 +56,43 @@ class ManagerNotification {
 		// Error, Other, Success, Warning
 	
 		// Perform checks on message content and change css for the given type of notification
-		if (typeOfMessage == "Error") {
-			if (messageContent == null || messageContent == "") {
-				messageContent = "There was an error but not message content was specified.";
+		if (typeOfMessage == 'Error') {
+			if (messageContent == null || messageContent == '') {
+				messageContent = 'There was an error but no message content was specified.';
 			}
-			$(".notification-bar").css({"background-color":"#ff1a1a", "background-image":"linear-gradient(0deg, #660000 -40%, #ff0000 140%)",
-										"box-shadow":"0 4px 8px 0 #1a0000, 0 6px 20px 0 #1a0000", "color":"#ffffff"});
-		} else if (typeOfMessage == "Other") {
-			if (messageContent == null || messageContent == "") {
-				messageContent = "No message content specified.";
+		} else if (typeOfMessage == 'Other') {
+			if (messageContent == null || messageContent == '') {
+				messageContent = 'No message content specified.';
 			}
-			$(".notification-bar").css({"background-color":"#ff1a1a", "background-image":"linear-gradient(0deg, #333333 -40%, #808080 140%)",
-										"box-shadow":"0 4px 8px 0 #0d0d0d, 0 6px 20px 0 #0d0d0d", "color":"#ffffff"});
-		} else if (typeOfMessage == "Success") {
-			if (messageContent == null || messageContent == "") {
-				messageContent = "It was successful but no message content was specified.";
+		} else if (typeOfMessage == 'Success') {
+			if (messageContent == null || messageContent == '') {
+				messageContent = 'It was successful but no message content was specified.';
 			}
-			$(".notification-bar").css({"background-color":"#78c200", "background-image":"linear-gradient(0deg, #3f6600 -40%, #9dff00 140%)",
-										"box-shadow":"0 4px 8px 0 #101a00, 0 6px 20px 0 #101a00", "color":"#ffffff"});
-		} else if (typeOfMessage == "Warning") {
-			if (messageContent == null || messageContent == "") {
-				messageContent = "There may be issues but no message content was specified.";
+		} else if (typeOfMessage == 'Warning') {
+			if (messageContent == null || messageContent == '') {
+				messageContent = 'There may be issues but no message content was specified.';
 			}
-			$(".notification-bar").css({"background-color":"#ff9933", "background-image":"linear-gradient(0deg, #e67300 -40%, #ffbf80 140%)", 
-										"box-shadow":"0 4px 8px 0 #994d00, 0 6px 20px 0 #994d00", "color":"#000000"});
 		}
 	
 		// Reset notification content to the given content and prepend a title
-		if (typeOfMessage == "Error" || typeOfMessage == "Other" || typeOfMessage == "Success" || typeOfMessage == "Warning") {
-			$(".notification-bar").html("<h3 class=\"notification-bar-title\">" + typeOfMessage + "</h3>");
+		if (typeOfMessage == 'Error' || typeOfMessage == 'Other' || typeOfMessage == 'Success' || typeOfMessage == "Warning") {
+			typeOfMessage = typeOfMessage.toLowerCase();
+
+			// Format for bootstrap (We changed over to bootstrap after this was made)
+			// Note: No need to worry about success or warning as they are already implemented into bootstrap
+			if (typeOfMessage == 'error') {
+				typeOfMessage = 'danger';
+			} else if (typeOfMessage == 'other') {
+				typeOfMessage = 'info';
+			}
+			//$('.notification-bar').html("<h3 class=\"notification-bar-title\">" + typeOfMessage + "</h3>");
+			$('.notification-bar').append('<div class="alert alert-' + typeOfMessage + '" alert-dismissible fade show" role="alert">'
+										  + messageContent + '<button type="button" class="close" onclick="notification_manager.hideNotification()">'
+										  + '	<span aria-hidden="true">&times;</span>'
+										  + '</button>'
+										  + '</div>');
+
 		}
-		$(".notification-bar").append(messageContent);
-	
-		// Add notification exit button (or link with 'x' char?)
-		$(".notification-bar").prepend("<a class=\"notification-bar-exit\" onclick=\"notification_manager.hideNotification()\">X</a>");
 
 		// Log notification to database
 		// TODO: Notifications need a web address at notification time, timestamp, and user agent
@@ -103,22 +106,22 @@ class ManagerNotification {
 		var seconds = date.getUTCSeconds();
 		var tz_offset = date.getTimezoneOffset();
 		// Convert client time to UTC time
-		var sign = tz_offset > 0 ? "-" : "+";
+		var sign = tz_offset > 0 ? '-' : '+';
 
 		var tz_offset_hours = this.date_pad(Math.floor(Math.abs(tz_offset)/60));
 		var tz_offset_minutes = this.date_pad(Math.abs(tz_offset)%60);
 
-		var tz_complete_offset = sign + tz_offset_hours + ":" + tz_offset_minutes + ":00";
-		var full_date = this.date_pad((month + 1)) + "/" + this.date_pad(day) 
-						+ "/" + year + " " + hour + ":" + minutes + ":" 
-						+ seconds + " " + tz_complete_offset;
+		var tz_complete_offset = sign + tz_offset_hours + ':' + tz_offset_minutes + ':00';
+		var full_date = this.date_pad((month + 1)) + '/' + this.date_pad(day) 
+						+ '/' + year + ' ' + hour + ':' + minutes + ':'
+						+ seconds + ' ' + tz_complete_offset;
 
 		// Get current web address
-		var current_web_address = "None"
+		var current_web_address = 'none'
 		try {
 			current_web_address = window.location;
 		} catch {
-			console.log("[Error]: Couldn't get current web address for notification log.");
+			console.log('[Error]: Couldn\'t get current web address for notification log.');
 		}
 		var data = {
 			notif_user_agent: navigator.userAgent,
@@ -130,12 +133,12 @@ class ManagerNotification {
 		
 		// Send information to servlet
 		$.ajax({
-			type: "POST",
-			url: "/loggingservlet/Servlet",
+			type: 'POST',
+			url: '/loggingservlet/Servlet',
 			data: JSON.stringify(data),
-			dataType: "json",
-			success: function(){console.log("Notification logged to node server.");},
-			contentType: "application/json"
+			dataType: 'json',
+			success: function(){console.log('Notification logged to node server.');},
+			contentType: 'application/json'
 		});
 	}	
 	
@@ -146,20 +149,20 @@ class ManagerNotification {
 	showNotification(type, content) {
 		// A function that displays and animates a given notification
 	
-		$(".notification-bar").css("display", "block");
+		$('.notification-bar').css('display', "block");
 		this.modifyNotification(type, content);
-		if (type == "Error") {
+		if (type == 'Error') {
 			console.error("[Notification]: " + type + " - " + content);
-		} else if (type == "Other") {
+		} else if (type == 'Other') {
 			console.info("[Notification]: " + type + " - " + content);
-		} else if (type == "Success") {
+		} else if (type == 'Success') {
 			console.info("[Notification]: " + type + " - " + content);
 		} else if (type == "Warning") {
 			console.warn("[Notification]: " + type + " - " + content);
 		}
 	
 		// Slide bar into view after displaying it
-		$(".notification-bar").animate({bottom: "0%"}, this.notification_speed);
+		$('.notification-bar').animate({bottom: '0%'}, this.notification_speed);
 		this.notification_toggle = true;
 	}	
 	
@@ -167,9 +170,9 @@ class ManagerNotification {
 		// A function, when called, hides a displayed notification.
 	
 		// Slide away bar after removing it
-		$(".notification-bar").animate({bottom: "-30%"}, this.notification_speed);
+		$('.notification-bar').animate({bottom: '-30%'}, this.notification_speed);
 		setTimeout(function() {
-			$(".notification-bar").css("display", "none");
+			$('.notification-bar').css('display', 'none');
 		}, this.notification_speed);
 	
 		// Make sure the queue doesn't try and show another notification while one
@@ -177,13 +180,14 @@ class ManagerNotification {
 		var that = this;
 		setTimeout(function() {
 			that.notification_toggle = false;
+			$('.notification-bar').html('');
 		}, this.notification_speed);
 	
 		// Remove notification after hidden
 		this.notification_queue.shift();  // Pop first element
 	
 		// Update 'In-Queue' dropdown item in notification tab
-		$("#notification-in-queue").html("In Queue (" + this.notification_queue.length + ")");
+		$('#notification-in-queue').html('In Queue (' + this.notification_queue.length + ')');
 	}
 
 	date_pad(n) {
--- scripts/menu-styles.js
+++ scripts/menu-styles.js
@@ -6,6 +6,27 @@
 function pageLoad() {
 	// A function to run once the web page loads.
 	
+	$(function() {
+		// ------------------------------------------------------- //
+		// Multi Level dropdowns
+		// ------------------------------------------------------ //
+		$("ul.dropdown-menu [data-toggle='dropdown']").on("click", function(event) {
+			event.preventDefault();
+			event.stopPropagation();
+	
+			$(this).siblings().toggleClass("show");
+	
+	
+			if (!$(this).next().hasClass('show')) {
+				$(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+				}
+				$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+				$('.dropdown-submenu .show').removeClass("show");
+			});
+	
+		});
+	});
+
 	// NOTE: Make sure any new sub-tabs are added below
 	var sub_tab_list = ["markers", "del-marker"];
 	sub_tab_list.forEach(handleSubTabs);
