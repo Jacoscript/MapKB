89557fd2862b41037aa46f52eb03fc33dcf57905
Fry, Tanner (Contractor) Edward <tfry@contractor.usgs.gov>
2020-04-08 17:34:14 -0500

    +- Added back the multi-level dropdown function block after accidently deleting it, rendering the nav menu dropdowns useless.

--- scripts/menu-styles.js
+++ scripts/menu-styles.js
@@ -5,7 +5,26 @@
 
 function pageLoad() {
 	// A function to run once the web page loads.
-
+	$(function() {
+		// --------------------- //
+		// Multi Level dropdowns
+		// --------------------- //
+		$("ul.dropdown-menu [data-toggle='dropdown']").on("click", function(event) {
+			event.preventDefault();
+			event.stopPropagation();
+	
+			$(this).siblings().toggleClass("show");
+	
+	
+			if (!$(this).next().hasClass('show')) {
+				$(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+				}
+				$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+				$('.dropdown-submenu .show').removeClass("show");
+			});
+	
+		});
+	});
 }
 
 $(document).ready(function() {
